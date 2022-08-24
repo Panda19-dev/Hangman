818ef9ab3705d0dabbf0a274686a992e56016efb
Oliver Ager Wiklander <agerwiklander.oliver@elev.ntig.se>
2022-08-24 11:06:36 +0200

    tg7ug

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -3,3 +3,4 @@ Console.WriteLine("Sup bitch");
 System.Console.WriteLine("El ordenador es lo mas importante sobra la vida porque los individuales estan encima de la internet. Mi amigo es un girafe y tiene tres llaves.");
 System.Console.WriteLine("Tengo un viritual individualidad tambien, pero yo soy un grande hombre con pantalones y un arma de balas.");
 System.Console.WriteLine("Tienes unos viritual individuales y como es tu larve");
+System.Console.WriteLine("hola senora estas mal?");
