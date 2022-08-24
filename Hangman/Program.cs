2369b1867cb578e3bd4be4e6d7151d2863da28e3
Jonatan Liljeblad <jonatan.liljeblad@live.com>
2022-08-24 10:40:29 +0200

    Panda

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Sup bitch");
 System.Console.WriteLine("Hej hur mår du?");
+System.Console.WriteLine("hello tilda!");
