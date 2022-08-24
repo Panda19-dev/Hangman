cddd06fba55206c271bcc47a174872aa74849aa7
Oliver Ager Wiklander <agerwiklander.oliver@elev.ntig.se>
2022-08-24 10:47:54 +0200

    Espania

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Sup bitch");
-System.Console.WriteLine("Hej hur mår du?");
+System.Console.WriteLine("El ordenador es lo mas importante sobra la vida porque los individuales estan encima de la internet. Mi amigo es un girafe y tiene tres llaves.");
+System.Console.WriteLine("Tengo un viritual individualidad tambien, pero yo soy un grande hombre con pantalones y un arma de balas.");
+System.Console.WriteLine("Tienes unos viritual individuales y como es tu larve");
